5e3b0103407de65fb711cbcbae25d33047f07867
PanagopoulosNikolaos <nikolaos0619@gmail.com>
2025-05-23 15:55:05 +0300

    new file:   Semester_2/C/File_Handling/exercise15 	modified:   Semester_2/C/File_Handling/exercise15.c

--- Semester_2/C/File_Handling/exercise15.c
+++ Semester_2/C/File_Handling/exercise15.c
@@ -8,10 +8,26 @@
 int main(){
     int character;
     int linesCount = 0;
-    int isEmpty = 1;
+    int isEmpty = 0;
     FILE *file = fopen("greeting.txt", "r");
     if (file == NULL) {
         perror("Error opening file");
         return EXIT_FAILURE;
     }
- 
+    while ((character = fgetc(file)) != EOF){
+        if (character == '\n'){
+            linesCount++;
+        }
+        if (linesCount == 0 && character == EOF){
+            isEmpty = 1;
+        }
+       
+    }
+    fclose(file);
+    if (isEmpty){
+        printf("The file is empty.\n");
+    } else {
+        printf("Total number of lines in the file: %d\n", linesCount);
+    }
+    return 0;
+}
